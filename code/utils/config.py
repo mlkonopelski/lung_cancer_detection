1f4a842925fcad9fbc3af9b341b6e0db14fe991e
Mateusz Konopelski <ml.konopelski@gmail.com>
2023-06-12 20:14:52 -0600

    Control Training and script variables using yml config

---
+++ code/utils/config.py
@@ -0,0 +1,31 @@
+from typing import Dict
+import yaml
+
+class General:
+    def __init__(self, general_dict: Dict) -> None:
+        self.device = general_dict['device']
+        
+        
+class Training:
+    def __init__(self, training_dict: Dict) -> None:
+        self.epochs = training_dict['epochs']
+        self.num_workers = training_dict['num_workers']
+        self.batch_size = training_dict['batch_size']
+        
+class Config:
+    def __init__(self, conf_yaml: Dict) -> None:
+        self.general = General(conf_yaml['general'])
+        self.training = Training(conf_yaml['training'])
+        
+    # TODO: Implement a nice __str__ method
+
+        
+with open("config.yml", "r") as f:
+    config = yaml.safe_load(f)
+    
+CONFIG = Config(config)
+
+
+if __name__ == '__main__':
+    
+    print(CONFIG.general.device)
