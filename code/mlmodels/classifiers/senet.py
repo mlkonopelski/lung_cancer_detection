560147526770aa5313eb5bc8b617f894434d7703
Mateusz Konopelski <ml.konopelski@gmail.com>
2023-10-29 17:13:30 -0500

    feat: Instance module with activation func as variable because EN will use SiLU instead of ReLU

--- code/mlmodels/classifiers/senet.py
+++ code/mlmodels/classifiers/senet.py
@@ -4,18 +4,19 @@ import torch.nn.functional as F
 
 
 class SENet(nn.Module):
-    def __init__(self, in_features: int, s_ratio: float) -> None:
+    def __init__(self, in_features: int, s_ratio: float, activation: nn.Module = nn.ReLU) -> None:
         super().__init__()
-
+        squeeze_features = max(1, in_features // s_ratio)
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.squeeze_conv = nn.Conv2d(in_features, in_features // s_ratio, kernel_size=1, padding=0)
-        self.expand_conv = nn.Conv2d(in_features // s_ratio, in_features, kernel_size=1, padding=0)
-
+        self.squeeze_conv = nn.Conv2d(in_features, squeeze_features, kernel_size=1, padding=0)
+        self.expand_conv = nn.Conv2d(squeeze_features, in_features, kernel_size=1, padding=0)
+        self.activation = activation()
+        
     def forward(self, x: torch.Tensor) -> torch.Tensor:
 
         out = self.avg_pool(x)
         out = self.squeeze_conv(out)
-        out = F.relu(out)
+        out = self.activation(out)
         out = self.expand_conv(out)
         out = F.sigmoid(out)
 
